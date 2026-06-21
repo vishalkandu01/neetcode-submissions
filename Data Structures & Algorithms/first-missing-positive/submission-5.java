b2b7f61df198731c12fa66bf8c7dcc7569b14baf
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2026-06-21 10:00:33 +0530

    Add: first-missing-positive - submission-5

---
+++ Data Structures & Algorithms/first-missing-positive/submission-5.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        // Arrays.sort(nums);
+        // int missingPositiveInteger = 1;
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (i > 0 && nums[i] == nums[i - 1]) continue;
+        //     if (nums[i] > 0 && nums[i] == missingPositiveInteger) {
+        //         missingPositiveInteger++;
+        //     } else if (nums[i] > 0) {
+        //         return missingPositiveInteger;
+        //     }
+        // }
+
+        // return missingPositiveInteger;
+
+
+
+
+        // Set<Integer> set = new HashSet<>();
+        // int n = nums.length;
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] > 0) {
+        //         set.add(nums[i]);
+        //     }
+        // }
+
+        // int missingPositiveInteger = 1;
+        // // for (int ele : set) {
+        // //     if(set.contains(missingPositiveInteger)) {
+        // //         missingPositiveInteger++;
+        // //     } else {
+        // //         return missingPositiveInteger;
+        // //     }
+        // // }
+
+        // while (set.contains(missingPositiveInteger)) {
+        //     missingPositiveInteger++;
+        // }
+
+        // return missingPositiveInteger;
+
+
+
+
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int correctIndex = nums[i] - 1;
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+}
