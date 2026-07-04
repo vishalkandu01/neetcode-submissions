60b7a277de533c8f562f704247be0deab7fc5036
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2026-07-04 15:49:33 +0530

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        int n = operations.length;
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (operations[i].equals("+")) {
+                int lastEle = list.get(list.size() - 1);
+                int lastSecondEle = list.get(list.size() - 2);
+                list.add(lastEle + lastSecondEle);
+            } else if (operations[i].equals("C")) {
+                list.remove(list.size() - 1);
+            } else if (operations[i].equals("D")) {
+                int lastEle = list.get(list.size() - 1);
+                list.add(lastEle * 2);
+            } else {
+                list.add(Integer.parseInt(operations[i]));
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < list.size(); i++) {
+            res += list.get(i);
+        }
+
+        return res;
+    }
+}
