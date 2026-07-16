0d81cf8fb204544908bc0a4228f3be0070c881f1
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2026-07-16 20:42:37 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,81 @@
+// class MinStack {
+//     private Stack<List<Integer>> stack;
+
+//     public MinStack() {
+//         stack = new Stack<>();
+//     }
+    
+//     public void push(int val) {
+//         List<Integer> list = new ArrayList<>();
+//         // if (stack.isEmpty()) {
+//         //     list.add(val, val);
+//         // } else {
+//         //     if (val < getMin()) {
+//         //         list.add(val, val);
+//         //     } else {
+//         //         list.add(val, getMin());
+//         //     }
+//         // }
+//         list.add(val);
+//         if (stack.isEmpty() || val < getMin()) {
+//             list.add(val);
+//         } else {
+//             list.add(getMin());
+//         }
+//         stack.push(list);
+//     }
+    
+//     public void pop() {
+//         stack.pop();
+//     }
+    
+//     public int top() {
+//         return stack.peek().get(0);
+//     }
+    
+//     public int getMin() {
+//         return stack.peek().get(1);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+class MinStack {
+    private Stack<int[]> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        int[] pair = new int[2];
+
+        pair[0] = val;
+        if (stack.isEmpty() || val < getMin()) {
+            pair[1] = val;
+        } else {
+            pair[1] = getMin();
+        }
+        stack.push(pair);
+    }
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek()[0];
+    }
+    
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
