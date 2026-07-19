856c5454f5b871b28ddb294c20ab7b7a4ae2fbec
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2026-07-19 06:34:30 +0530

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+
+        int[][] cars = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+
+        Arrays.sort(cars, (a, b) -> Integer.compare(a[0], b[0]));
+
+        Stack<Double> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            double time = (double)(target - cars[i][0]) / cars[i][1];
+
+            if (stack.isEmpty() || time > stack.peek()) {
+                stack.push(time);
+            }
+        }
+
+        return stack.size();
+    }
+}
