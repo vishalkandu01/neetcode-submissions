dbed89df31b22b792f40df20acf55841877e4fa3
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2026-07-21 00:21:53 +0530

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> stack = new ArrayDeque<>();
+        String[] parts = path.split("/");
+
+        for (String part : parts) {
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            } else if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(part);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            result.append("/").append(stack.removeLast());
+        }
+
+        return result.length() == 0 ? "/" : result.toString();
+    }
+}
