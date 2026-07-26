74afa081a653b526a09de83e62448d0c8b14cc64
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2026-07-27 00:57:21 +0530

    Add: decode-string - submission-0

---
+++ Data Structures & Algorithms/decode-string/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+
+        StringBuilder curr = new StringBuilder();
+        int num = 0;
+
+        for (char ch : s.toCharArray()) {
+            if  (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            } else if (ch == '[') {
+                countStack.push(num);
+                stringStack.push(curr);
+                num = 0;
+                curr = new StringBuilder();
+            } else if (ch == ']') {
+                int repeat = countStack.pop();
+                StringBuilder prev = stringStack.pop();
+
+                for (int i = 0; i < repeat; i++) {
+                    prev.append(curr);
+                }
+
+                curr = prev;
+            } else {
+                curr.append(ch);
+            }
+        }
+        return curr.toString();
+    }
+}
